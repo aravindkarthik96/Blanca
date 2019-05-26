15f10b677208451fb83bc16e2b81d206f6c9ca8a
aravind.karthik <aravind.karthik@go-jek.com>
2019-05-26 13:24:17 +0530

    setup sceneform view

--- app/src/main/java/com/aravindkarthik/blanca/HomeActivity.kt
+++ app/src/main/java/com/aravindkarthik/blanca/HomeActivity.kt
@@ -4,16 +4,25 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.ContentValues.TAG
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val MIN_OPENGL_VERSION = 3.0
 
 class HomeActivity : AppCompatActivity() {
+
+    private var testModel: ModelRenderable? = null
+    private var arFragment: ArFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +31,39 @@ class HomeActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main)
+        arFragment = supportFragmentManager.findFragmentById(R.id.canvasView) as ArFragment
+
+
+        ModelRenderable.builder()
+            .setSource(this, R.raw.andy)
+            .build()
+            .thenAccept { renderable -> testModel = renderable }
+            .exceptionally { _ -> null }
+
 
         run_code.setOnClickListener {
             interpretCode()
         }
 
+//        canvasView.setOnTapArPlaneListener(
+//            { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
+//                if (testModel == null) {
+//                    return@arFragment.setOnTapArPlaneListener
+//                }
+//
+//                // Create the Anchor.
+//                val anchor = hitResult.createAnchor()
+//                val anchorNode = AnchorNode(anchor)
+//                anchorNode.setParent(arFragment.getArSceneView().getScene())
+//
+//                // Create the transformable andy and add it to the anchor.
+//                val andy = TransformableNode(arFragment.getTransformationSystem())
+//                andy.setParent(anchorNode)
+//                andy.renderable = testModel
+//                andy.select()
+//            })
+
+//        canvasView.setupSession()
     }
 
     private fun interpretCode() {
