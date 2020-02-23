8e88db1340021e4a4272525edc80be2df06cb1e9
aravind.karthik <aravind.karthik@go-jek.com>
2020-02-23 16:32:50 +0700

    added click handling for editor and logger tabs

--- app/src/main/java/com/aravindkarthik/blanca/BlancaHomeActivity.kt
+++ app/src/main/java/com/aravindkarthik/blanca/BlancaHomeActivity.kt
@@ -1,12 +1,29 @@
 package com.aravindkarthik.blanca
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_blanca_home.*
 
 class BlancaHomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blanca_home)
+    }
+
+    fun handleEditorTitleClick(view: View) {
+        loggerContainer.visibility = View.GONE
+        editorContainer.visibility = View.VISIBLE
+        loggerTitle.setTextColor(getColor(R.color.blanca_text_de_selected))
+        editorTitle.setTextColor(getColor(R.color.blanca_text_selected))
+    }
+
+    fun handleLoggerTitleClick(view: View) {
+        loggerContainer.visibility = View.VISIBLE
+        editorContainer.visibility = View.GONE
+        loggerTitle.setTextColor(getColor(R.color.blanca_text_selected))
+        editorTitle.setTextColor(getColor(R.color.blanca_text_de_selected))
+
     }
 }
